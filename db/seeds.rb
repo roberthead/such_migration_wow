c60b034f4c7bb6f43c8174a4a4af6ddd2f595595
Rob Head <pair+rob@pacerpro.com>
2015-07-30 16:31:31 -0700

    Add Deck seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Deck.where(slug: "zero-downtime-migrations").first_or_create do |deck|
+  deck.update({
+    name: "Zero Downtime Migrations",
+    description: "Roll out database schema changes and data migrations to a Ruby on Rails application without production downtime.",
+    author_name: "Robert Head"
+  })
+end
