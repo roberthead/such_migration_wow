0eb2a5b41703e5fbe982e4f3a8bfdc8de421927a
Rob Head <pair+rob@pacerpro.com>
2015-07-30 19:05:54 -0700

    Allow slides to be id'd by position

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,6 +1,6 @@
 class SlidesController < ApplicationController
   def show
     @deck = Deck.find_by(slug: params[:deck_id])
-    @slide = @deck.slides.find_by(slug: params[:id]) || @deck.slides.find(params[:id])
+    @slide = @deck.slides.find_by(slug: params[:id]) || @deck.slides.find_by(position: params[:id])
   end
 end
