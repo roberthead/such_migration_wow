7ca7cd849d70663673b1688e8f7dc627a2544b51
Rob Head <pair+rob@pacerpro.com>
2015-07-30 19:05:35 -0700

    Add redcarpet and foundation-rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def markdown(markdown_text)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
+        # no_intra_emphasis: true,
+        # fenced_code_blocks: true,
+        # disable_indented_code_blocks: true
+      }
+    )
+    @markdown.render(markdown_text).html_safe
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,59 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 deck = Deck.where(slug: "zero-downtime-migrations").first_or_create do |deck|
-  deck.update({
-    name: "Zero Downtime Migrations",
-    description: "Roll out database schema changes and data migrations to a Ruby on Rails application without production downtime.",
-    author_name: "Robert Head"
-  })
+  deck.name = "Strategies for Zero Downtime Migrations"
+  deck.description = "Roll out database schema changes and data migrations to a Ruby on Rails application without production downtime."
+  deck.author_name = "Robert Head"
 end
+
+deck.slides.destroy_all
 
 slide_attributes_list = [
   {
-    header: "Zero Downtime Migrations",
-    body: <<-EOS.gsub(/\A\s*/, ''),
-        Robert Head
+    header: "Strategies for Zero Downtime Migrations",
+    body: <<-EOS.gsub(/^ {6}/, ''),
+      ### Robert Head
+
+      ### UserTesting.com
+    EOS
+    speaker_notes: <<-EOS.gsub(/^ {6}/, ''),
+      Welcome.
+    EOS
+  },
+
+  {
+    header: "Principles for Deployments",
+    body: <<-EOS.gsub(/^ {6}/, ''),
+      ### Migrations must be:
+
+      - Reversible
+      - Restrained
+
+      ### Code must be:
+
+      - Flexible
       EOS
-    speaker_notes: "",
-  }
+    speaker_notes: <<-EOS.gsub(/^ {6}/, ''),
+      ## Reversible?
+
+      - Strive to use the reversible migration actions.
+      - Add additional creative attributes to destructive actions in the change method
+
+      ## Restrained?
+
+      - Constrained to migration actions
+      - Schema changes only
+      - No data migrations
+      - No references to external classes
+      - SQL when necessary
+    EOS
+  },
 ]
 
 slide_attributes_list.each_with_index do |slide_attributes, index|
-  Slide.where(slide_attributes.merge(deck_id: deck.id)).first_or_initialize do |slide|
+  deck.slides.where(slide_attributes).first_or_initialize do |slide|
     slide.position = index + 1
+    slide.deck = deck
     slide.save
   end
 end
