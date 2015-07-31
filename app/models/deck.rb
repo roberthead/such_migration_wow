107e29ba5cb7dc3041aac80aed83689f1d872aad
Rob Head <pair+rob@pacerpro.com>
2015-07-31 11:34:24 -0700

    Add slide placement.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,10 +1,20 @@
 class Deck < ActiveRecord::Base
-  has_many :slides, -> { order(:position) }, dependent: :destroy
+  # TODO After next deployment, add me and remove slides() method
+  # has_many :slides, through: :slide_placements, -> { order 'slide_placements.position' }
 
   before_validation :compute_slug
 
   def to_param
     slug
+  end
+
+  def slides
+    if SlidePlacement.count(deck_id: id)
+      placement_ids = SlidePlacement.where(deck_id: id).pluck(:id)
+      Slide.where(id: placement_ids)
+    else
+      Slide.where(deck_id: id).order(:position)
+    end
   end
 
   private
